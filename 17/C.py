7e809c5cc0eccddbefa6b2bcb3139d22ab812352
student <ll@g>
2024-04-19 10:13:57 +1200

    5486

--- 17/C.py
+++ 17/C.py
@@ -1,13 +1,14 @@
-s = input()
+s = input(' '.join).split(' ')
 t = 0
 m = 0
+l = ''
 f = False
 
-for i in range(len(s)):
-    if s[i] != ' ':
+for i in s:
+    for j in i:
         t += 1
-    elif s[i] == ' ':
-        if t > m:
-            m = t
-        t = 0
-print(m)
+    if m < t:
+        m = t
+        l = i
+    t = 0
+print(f'Самое длинное слово: {l}, длина {m}')
---
+++ 17/С2.py
@@ -0,0 +1,7 @@
+s = input(' '.join).split('and')
+s1 = ''
+for i in range(len(s)-1):
+    d = s[i]+'or'
+    s1 += d
+s1 += s[len(s)-1]
+print(s1)
