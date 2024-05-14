4e0ed5d68a8c0b5b2d058e65b5d83543cc1ea2e4
student <ll@g>
2024-05-14 12:14:21 +1200

    5486

--- 18/D.py
+++ 18/D.py
@@ -1,18 +1,18 @@
 def A(s):
-    n=[]
-    k=[]
-    i=0
-    while i<len(s):
+    n = []
+    k = []
+    i = 0
+    while i < len(s):
         if s[i].isdigit():
             num = 0
-            while i<len(s) and s[i].isdigit():
-                num = num*10+int(s[i])
+            while i < len(s) and s[i].isdigit():
+                num = num * 10 + int(s[i])
                 i += 1
             n.append(num)
         elif s[i] == "(":
             k.append(s[i])
-            i+=1
-        elif s[i] ==")":
+            i += 1
+        elif s[i] == ")":
             while k[-1] != '(':
                 num2 = n.pop()
                 num1 = n.pop()
@@ -26,9 +26,9 @@ def A(s):
                 elif op == "/":
                     n.append(num1 // num2)
             k.pop()
-            i+=1
-        elif s[i] in ['+','-','*','/']:
-            while k and k[-1] in ['*','/'] and ['+','-']:
+            i += 1
+        elif s[i] in ['+', '-', '*', '/']:
+            while k and k[-1] in ['*', '/'] and ['+', '-']:
                 num2 = n.pop()
                 num1 = n.pop()
                 op = k.pop()
@@ -51,6 +51,8 @@ def A(s):
         elif op == "/":
             n.append(num1 // num2)
     return n[0]
+
+
 s = input('Введите выражение: ')
 result = A(s)
-print('Ответ:',result)
+print('Ответ:', result)
