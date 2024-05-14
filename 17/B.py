0f9b919e4586b8f8f1117d277138e5cd1552b3dc
student <ll@g>
2024-05-14 12:05:19 +1200

    5486

---
+++ 17/B.py
@@ -0,0 +1,3 @@
+s = input(' '.join).split('/')
+for i in s:
+    print(i)
--- 17/С2.py
+++ 17/С2.py
@@ -1,7 +1,7 @@
 s = input(' '.join).split('and')
 s1 = ''
 for i in range(len(s)-1):
-    d = s[i]+'or'
+    d = s[i]+'&'
     s1 += d
 s1 += s[len(s)-1]
 print(s1)
