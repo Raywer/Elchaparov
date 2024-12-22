6d31b832e8eddd02748da693efb0f289c1d383ca
student <ll@g>
2024-12-23 13:48:41 +1400

    5486

--- 11 класс/19-21/5375.py
+++ 11 класс/19-21/5375.py
@@ -7,5 +7,5 @@ def game(s, step):
     return any(h) if (step-1)%2==0 else all(h)
 
 print('19)',[s for s in range(1,165) if not(game(s,1)) and (game(s,2))])
-print('19)',[s for s in range(1,165) if not(game(s,1)) and (game(s,3))])
+print('20)',[s for s in range(1,165) if not(game(s,1)) and (game(s,3))])
 print('21)',[s for s in range(1,165) if not(game(s,2)) and game(s,4)])
